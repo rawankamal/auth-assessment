719dad1c95f2a40adeac971dde7ffa23e9338014
Rawan <rwaankamal@gmail.com>
2025-09-15 16:19:14 +0300

    deploy on 8080

--- apps/api/src/app/users/users.module.ts
+++ apps/api/src/app/users/users.module.ts
@@ -3,15 +3,17 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
-import { AuthModule } from '../auth/auth.module'; // ADD THIS IMPORT
+import { AuthModule } from '../auth/auth.module'; 
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    AuthModule // ADD THIS LINE - Import AuthModule for JWT strategy
+    AuthModule, // Import AuthModule for JWT strategy
   ],
+
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
 })
-export class UsersModule { }
+export class UsersModule {}
+
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as path from 'path';
@@ -20,12 +21,12 @@ async function bootstrap() {
 
   // أي request مش API → رجّعه لـ index.html بتاع Angular
   const expressApp = app.getHttpAdapter().getInstance();
-expressApp.get(
-  /^(?!\/api).*/,
-  (req: express.Request, res: express.Response) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
-  }
-);
+  expressApp.get(
+    /^(?!\/api).*/,
+    (req: express.Request, res: express.Response) => {
+      res.sendFile(path.join(frontendPath, 'index.html'));
+    }
+  );
 
   const port = process.env['PORT'] || 3000;
   await app.listen(port, '0.0.0.0');
